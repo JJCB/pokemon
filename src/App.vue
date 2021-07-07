32d6fb5ca130d94497fb9be1011ed448721e9324
Jhon Castro <jhon.castro.blas@gmail.com>
2021-07-07 16:15:23 -0500

    feat: se añade vue router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app">
-    <Welcome />
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Welcome from "./views/Welcome.vue";
-export default {
-  name: "App",
-  components: {
-    Welcome,
-  },
-};
-</script>
 
 <style lang="scss" >
+
 @import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900");
 #app {
   font-family: $lato;
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Welcome from '../views/Welcome.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: Welcome
+  },
+  {
+    path: '/pokemon',
+    name: 'Pokemon',
+    component: () => import(/* webpackChunkName: "pokemon" */ '../views/Pokemon.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
