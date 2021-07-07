754dcef07729b890c0407f7536ea00336ec72ffc
Jhon Castro <jhon.castro.blas@gmail.com>
2021-07-07 10:06:15 -0500

    feat: se configura scss

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Welcome />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Welcome from './views/Welcome.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Welcome
   }
 }
 </script>
 
-<style>
+<style >
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/views/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
